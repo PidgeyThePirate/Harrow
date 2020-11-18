a06169f0e43ff12f9a2012e473a9202b5de1d96f
Pidgey <35320204+PidgeyThePirate@users.noreply.github.com>
2020-11-18 17:33:36 +1100

    Update harrow.js

--- src/harrow.js
+++ src/harrow.js
@@ -6,29 +6,35 @@ let intelligenceButton = document.getElementById('intelligence')
 let wisdomButton = document.getElementById('wisdom')
 let charismaButton = document.getElementById('charisma')
 
-var biggestSize = 0
-if (strengthButton.width > biggestSize) {
-  biggestSize = strengthButton.width
-}
-if (dexterityButton.width > biggestSize) {
-  biggestSize = dexterityButton.width
-}
-if (constitutionButton.width > biggestSize) {
-  biggestSize = constitutionButton.width
-}
-if (intelligenceButton.width > biggestSize) {
-  biggestSize = intelligenceButton.width
-}
-if (wisdomButton.width > biggestSize) {
-  biggestSize = wisdomButton.width
-}
-if (charismaButton.width > biggestSize) {
-  biggestSize = charismaButton.width
-}
+window.onload = evenSize();
 
-strengthButton.width = biggestSize
-dexterityButton.width = biggestSize
-constitutionButton.width = biggestSize
-intelligenceButton.width = biggestSize
-wisdomButton.width = biggestSize
-charismaButton.width = biggestSize
+
+
+function evenSize(){
+  var biggestSize = 0
+  if (strengthButton.width > biggestSize) {
+    biggestSize = strengthButton.width;
+  }
+  if (dexterityButton.width > biggestSize) {
+    biggestSize = dexterityButton.width;
+  }
+  if (constitutionButton.width > biggestSize) {
+    biggestSize = constitutionButton.width;
+  }
+  if (intelligenceButton.width > biggestSize) {
+    biggestSize = intelligenceButton.width;
+  }
+  if (wisdomButton.width > biggestSize) {
+    biggestSize = wisdomButton.width;
+  }
+  if (charismaButton.width > biggestSize) {
+    biggestSize = charismaButton.width;
+  }
+
+  strengthButton.width = biggestSize;
+  dexterityButton.width = biggestSize;
+  constitutionButton.width = biggestSize;
+  intelligenceButton.width = biggestSize;
+  wisdomButton.width = biggestSize;
+  charismaButton.width = biggestSize;
+}
