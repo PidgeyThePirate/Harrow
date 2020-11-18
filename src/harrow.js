ebda2f2548882a12936455483294d68cf8f1b581
Pidgey <35320204+PidgeyThePirate@users.noreply.github.com>
2020-11-18 17:28:37 +1100

    Update harrow.js

--- src/harrow.js
+++ src/harrow.js
@@ -1,1 +1,34 @@
+let suitDiv = document.getElementById('selectSuit')
+let strengthButton = document.getElementById('strength')
+let dexterityButton = document.getElementById('dexterity')
+let constitutionButton = document.getElementById('constitution')
+let intelligenceButton = document.getElementById('intelligence')
+let wisdomButton = document.getElementById('wisdom')
+let charismaButton = document.getElementById('charisma')
 
+var biggestSize = 0
+if (strengthButton.width > biggestSize) {
+  biggestSize = strengthButton.width
+}
+if (dexterityButton.width > biggestSize) {
+  biggestSize = dexterityButton.width
+}
+if (constitutionButton.width > biggestSize) {
+  biggestSize = constitutionButton.width
+}
+if (intelligenceButton.width > biggestSize) {
+  biggestSize = intelligenceButton.width
+}
+if (wisdomButton.width > biggestSize) {
+  biggestSize = wisdomButton.width
+}
+if (charismaButton.width > biggestSize) {
+  biggestSize = charismaButton.width
+}
+
+strengthButton.width = biggestSize
+dexterityButton.width = biggestSize
+constitutionButton.width = biggestSize
+intelligenceButton.width = biggestSize
+wisdomButton.width = biggestSize
+charismaButton.width = biggestSize
